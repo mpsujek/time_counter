b28c7cef3df6ae05af1ae9b562acd5ece80415b0
Katarzyna Psujek <mateusz.psujek@gmail.com>
2017-03-19 17:00:07 +0100

    adding new functions and buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
+var ul = document.getElementById('list');
+var commentsArray = [];
+
+var commentsAfterLoad = localStorage.getItem('comment');
+// console.log(commentsAfterLoad);
+if (commentsAfterLoad === null) {
+    commentsArray = [];
+
+} else {
+    // console.log(commentsAfterLoad.split(','));
+    commentsArray = commentsAfterLoad.split(',');
+    for (var i = 0; i < commentsArray.length; i++) {
+      createList(ul, commentsArray[i]);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    var ul = document.getElementById('list');
+    // wczytanie danych z local storage
+
     var button = document.getElementById('make_point');
     var start = new Date();
     var date = new Date();
@@ -7,10 +24,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var minute = date.getMinutes();
     var second = date.getSeconds();
     var li = document.createElement('li');
+    var clearBtn = document.getElementById('clear_local');
+    var actualTime = document.getElementById('actualTime');
+
+
     li.appendChild(document.createTextNode("Actual Time - " + hour + ":" + minute + ":" + second));
     ul.appendChild(li);
-
-
+    actualTime.appendChild(document.createTextNode("Actual Time - " + hour + ":" + minute + ":" + second));
     button.onclick = function() {
         var date = new Date();
         var hour = date.getHours();
@@ -28,11 +48,30 @@ document.addEventListener("DOMContentLoaded", function() {
         var minutes = Math.floor(x % 60);
         x /= 60;
         var hours = Math.floor(x % 24);
-        var nextLi = document.createElement('li');
-        nextLi.appendChild(document.createTextNode("Actual time on click: " + hour + ":" + minute + ":" + second + " Time spend HH:MM:SS = " + hours + ":" + minutes + ":" + seconds));
-        ul.appendChild(nextLi);
+        var commentText = document.getElementById('comment').value;
+        // var text = document.createTextNode("Actual time on click: " + hour + ":" + minute + ":" + second + " Time spend - " + hours + " hours " + minutes + " min. " + seconds + " sec. " + "Comment: " + commentText);
 
+        var text = "Time on click: " + hour + ":" + minute + ":" + second + " Time spend - " + hours + " hours " + minutes + " min. " + seconds + " sec. " + "Comment: " + commentText;
+
+        createList(ul, text);
+
+        commentsArray.push(text);
+        localStorage.setItem('comment', commentsArray);
+
+        // var getItem = localStorage.getItem("row");
+        // console.log(getItem);
+        document.getElementById('comment').value = "";
         // console.log("Time spend HH:MM:SS = " + hours + ":" + minutes + ":" + seconds);
     };
 
+    clearBtn.onclick = function(){
+      localStorage.clear();
+      location.reload();
+    };
 });
+
+function createList(list, text) {
+  var listItem = document.createElement('li');
+  listItem.innerHTML = text;
+  list.appendChild(listItem);
+}
